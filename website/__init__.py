fd2dd202ff8364f9f065e4063117b706362b4ffe
kylewangg <kylewang2003@hotmail.com>
2024-05-05 22:46:47 -0700

    Tried to get the login and register to work

--- website/__init__.py
+++ website/__init__.py
@@ -27,6 +27,10 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     return app
 
 def create_database(app):
